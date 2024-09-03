bb947f1a3a0b0ef395cb73dd97745092e78e68c7
Ahmed-elsaid-AEA <ahmed122727727@gmail.com>
2024-09-03 22:18:48 +0300

    this my project

---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'dart:io';
+
+void main() {
+// var  dynamic
+  int firstNumber = 40;
+  int secondNumber = 20;
+  int sum = firstNumber + secondNumber;
+  print("sum : $sum");
+  /*
+  print("5");
+  //make program that allow user to enter name , age ,saalry fuctllty and his success , and his price of his pen
+  print("enter your age : ");
+  String age = stdin.readLineSync()!;
+  print("your age is : ${int.parse(age)+10}");*/
+  /* String name, fucalty;
+  int age, salary;
+  bool success;
+  double priceOfHisPen;
+  print("Enter Your name (String) :");
+  name = stdin.readLineSync()!;
+  print("enter Your faculty(string): ");
+  fucalty = stdin.readLineSync()!;
+  print("enter Your age(int): ");
+  age = int.parse(stdin.readLineSync()!);
+  print("enter Your salary(int): ");
+  salary = int.parse(stdin.readLineSync()!);
+  print("enter Your success(bool): ");
+  success = bool.parse(stdin.readLineSync()!);
+  print("enter Your price of your pen(double): ");
+  priceOfHisPen = double.parse(stdin.readLineSync()!);
+  print("your name is $name , your fucalty is $fucalty , your age is $age , your salary is $salary , your success is $success ,your price of pen : $priceOfHisPen");
+*/
+  /* String firstName = 'ahmed';
+  int age = 32;
+  String faculty = "computer science";
+  int salary = 100000;
+  bool success = true;
+  double priceOfYourPen = 10.5;
+  print("my first name : $firstName   ,  my age is : $age  , my faculty is : $faculty   , success : $success   , my price of pen : $priceOfYourPen");
+ */
+  /* String nameEmployee = "ali";
+  int salary = 10000;
+  double age = 60.5;
+  bool accepeted = false;
+  print("your name is " + nameEmployee);
+  print("your salary is $salary");
+  print("your age is $age");
+  print("is accepted ?  $accepeted");
+  print(
+      "name : $nameEmployee ,  salary : $salary ,   age : $age  accepted : $accepeted");
+  */
+  /*
+  int   1 2 3 
+  double  1 2 1.5 1.3
+  String ahmed moahmed alaa 
+  bool true false
+   */
+
+  // print /*dljhbdaoijfpodcx */ ("ahmed");
+  //inline comment
+/*
+
+multil line comment
+*/
+/*  print('ahmed         a');
+  print("wael          b");
+  print("ahmed" + " wael");
+  print(5 + 5);
+  print("5" + "  5");*/
+}
